05a7241e5a4ecd2ea7750000dacb36f7f0794b2a
ExpoCode Tech <expocodetech@jex-macbookpro-2.local>
2016-10-12 13:27:02 +0200

    Configuración inicial de proyecto Angular 2

---
+++ systemjs.config.js
@@ -0,0 +1,39 @@
+(function (global) {
+  System.config({
+    paths: {
+      // paths sirve como alias
+      'npm:': 'node_modules/'
+    },
+    // map le dice al cargador del Sistema donde buscar los componentes
+    map: {
+      // la app esta dentro de la carpeta de la aplicación
+      app: 'app',
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      // other libraries
+      'rxjs':                      'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+    },
+    // packages le dice al Cargador del Sistema como y cuando cargar un fichero con o sin extensión
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      },
+      'angular-in-memory-web-api': {
+        main: './index.js',
+        defaultExtension: 'js'
+      }
+    }
+  });
+})(this);
