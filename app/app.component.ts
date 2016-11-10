6c3e7fc26a6652fcaa8af1c1aa1a91c0a58f3a06
Javier Exposito <javiexpo2003@gmail.com>
2016-11-10 18:12:09 +0100

    Fixing comments

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,12 @@
  * OnInit: usado para implementar la interfaz OnInit y con ella el método ngOnInit()
  * 
  * Importamos de la librería rxjs/Rx el modulo:
- * Observable: usado para suscrubirnos a un Observable Timer
+ * Observable: usado para suscribirnos a un Observable Timer
 */
 import { Component, OnInit} from '@angular/core';
 import { Observable } from 'rxjs/Rx';
+
+//Importamos el servicio ListService del fichero app.service.ts  
 import { ListService } from './app.service';
 
 /** 
@@ -16,7 +18,8 @@ import { ListService } from './app.service';
  * y los siguientes parámetros:
  * selector = define el nombre del selector CSS usado por un elemento 
  *            HTML que representa al componente  
- * template = define la plantilla HTML que renderiza el contenido en pantalla 
+ * template = define la plantilla HTML que renderiza el contenido en pantalla
+ * providers = Permite definir los servicios que se van a usar en este componente 
 */
 @Component({
   selector: 'mi-app',
@@ -33,7 +36,7 @@ export class AppComponent  implements OnInit {
   //Titulo del Componente
   title = 'Mi Shopping List';
   
-  //Array de Items que se han de mostrar en pantalla 
+  //Declaramos el Array de Items que se mostrará en pantalla de tipo Product    
   items: Product[] = [];
 
   _myInterval = null;
@@ -43,42 +46,35 @@ export class AppComponent  implements OnInit {
     cantidad: 0
   }
 
-  ngOnInit(): void {
-    //ngOnInit es un método se ejecuta cuando el componente 
-    //esta completamente instanciado
-
-    this.listService.getItems().then(items => this.items = items);
-
-    /**
-     * Instanciamos un Observable.timer cuya ejecucion tenga 
-     * un retraso de 1 segundo (1000 milisegundos)
-     * y se re-ejecute cada 3 segundos (3000 milisegundos)
-     * y en cada ejecución llamará al método _pickRandomItems   
-     * */
-    /*let timer = Observable.timer(1000,3000);
-    timer.subscribe(t=> {
-        this._pickRandomItems();
-    });*/  
-  }
-
-  //Usamos los parámetros del contrucytor del componente para
-  // instanciar el o los servicios 
+  //Usamos los parámetros del constructor del componente para
+  // instanciar el o los servicios, en este caso inyectamos e
+  // instanciamos el servicio ListService, para poder hacer uso del mismo 
+  //en cualquier otro método de este componente 
   constructor(private listService: ListService) {
     
   }
 
+  ngOnInit(): void {
+    //ngOnInit es un método se ejecuta cuando el componente 
+    //esta completamente instanciado
+
+    //Usamos el método getItems() del ListService para obtener los items activos 
+    this.listService.getItems().then(items => this.items = items);  
+  }
+
   addProduct():void {
-    //Verificamos si el usuario ha introducido un nombre de producto
-    if (this.newProduct.productName === ""){
-      return;
-    }
-    //Verificamos si el usuario ha introducido una cantidad
-    if (this.newProduct.cantidad === 0){
+    //Verificamos si el usuario ha introducido un nombre de producto y una cantidad
+    if (this.newProduct.productName === "" || this.newProduct.cantidad === 0){
       return;
     }
     
+    //Usamos el método addProduct del Servcio ListService para almacenar el producto 
     this.listService.addProduct(this.newProduct).then(items => {
+      //Al resolverse la Promise actualizamos la propiedad items del componente
+      //con el parámetro items que nos devuelve la Promise resuelta en el método 
+      //addProduct del Servcio ListService 
       this.items = items
+      //Blanquemaos los imputs
       this.newProduct.cantidad = 0;
       this.newProduct.productName = "";
     });
@@ -86,6 +82,7 @@ export class AppComponent  implements OnInit {
   
 }
 
+//Declaramos el objeto de tipo Product para tipificar el Array de Items
 export class Product {
   productName: string;
   cantidad: number;
--- app/app.service.ts
+++ app/app.service.ts
@@ -1,6 +1,10 @@
+//Importamos el objeto Injectable de angular/core para poder hacer uso del Decorator
 import { Injectable } from '@angular/core';
+
+//Importamos el tipo de objeto Product el componente 
 import { Product } from './app.component';
 
+//Declaramos 
 @Injectable()
 export class ListService {
    //Mini BD de productos con sus cantidades
@@ -17,10 +21,17 @@ export class ListService {
         {productName: 'Pimientos', cantidad: 1}
         ];
 
+
+    //Listado de Items activos en la lista.
     items = [];
 
+    //Método Constructor sin parametros necesario para lograr la inyeccion e instanciamietno 
+    //del servicio en los componentes donde se necesite   
     constructor() { }
 
+    //Método para obtener del servicio los items activos en la lista
+    //Este método devuelve una Promises con lo cual podemos manejar de 
+    //ser necesario peticiones asincronas   
     getItems(): Promise<Product[]> {
       return Promise.resolve(this.items);
     }
@@ -36,6 +47,9 @@ export class ListService {
         }
     }
 
+    //Método para aregar items a la lista de items activos
+    //Este método devuelve una Promises con lo cual podemos manejar de 
+    //ser necesario peticiones asincronas
     addProduct(newProduct: Product): Promise<Product[]> {      
         //Creamos un nueva instancia del Product
         let newProd: Product = new Product();
@@ -44,6 +58,7 @@ export class ListService {
         newProd.cantidad = newProduct.cantidad;
         //Agregamos en nuevo producto a la lista
         this.items.push(newProd);
+        //resolvemos la Promise retornando los items activo incluyendo el item agreagdo
         return Promise.resolve(this.items);
     }
 }
