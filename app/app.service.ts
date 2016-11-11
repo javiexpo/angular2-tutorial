8f8452d3d297f540d854c2fa6cd168d60143c1c2
Javier Exposito <javiexpo2003@gmail.com>
2016-11-11 14:05:37 +0100

    Adding Angular2IndexedBD Service

--- app/app.service.ts
+++ app/app.service.ts
@@ -4,11 +4,22 @@ import { Injectable } from '@angular/core';
 //Importamos el tipo de objeto Product el componente 
 import { Product } from './app.component';
 
+//Importamos el serivio AngularIndexedDB del modulo angular2-indexeddb que hemos instalado 
+import {AngularIndexedDB} from '../node_modules/angular2-indexeddb/angular2-indexeddb';
+
 //Declaramos 
 @Injectable()
 export class ListService {
-   //Mini BD de productos con sus cantidades
-  itemsDB: Product[] = [
+    //Nombre de la BD
+    static DATABASE_NAME = 'shoppingListDB';
+    //Nombre del Store o Tabla de items
+    static ITEMS_TABLE = 'items';
+
+    //Isntancia del Servicio AngularIndexedDB 
+    db: AngularIndexedDB;
+
+    //Mini BD de productos con sus cantidades
+    itemsDB: Product[] = [
         {productName: 'Manzanas', cantidad: 5}, 
         {productName: 'Pera', cantidad: 2},
         {productName: 'Banana', cantidad: 1},
@@ -19,20 +30,39 @@ export class ListService {
         {productName: 'Queso', cantidad: 2},
         {productName: 'Tomates', cantidad: 5},
         {productName: 'Pimientos', cantidad: 1}
-        ];
+    ];
 
 
     //Listado de Items activos en la lista.
-    items = [];
+    items = [];  
 
     //Método Constructor sin parametros necesario para lograr la inyeccion e instanciamietno 
     //del servicio en los componentes donde se necesite   
-    constructor() { }
+    constructor() {
+        //Creamos la BD Indexed
+        this.db = new AngularIndexedDB(ListService.DATABASE_NAME, 1);
+
+        //Creamos un Store (especie de tabla) para almacenar los prouctos de la lista con sus cantidades 
+        // y un ID autoincremental que los identifique como unicos dentro de la Store 
+        this.db.createStore(1, (evt) => {
+            let objectStore = evt.currentTarget.result.createObjectStore(
+                ListService.ITEMS_TABLE, { keyPath: "id", autoIncrement: true });
+            objectStore.createIndex("productName", "productName", { unique: false });
+            objectStore.createIndex("cantidad", "cantidad", { unique: true });
+        });
+     }
 
     //Método para obtener del servicio los items activos en la lista
     //Este método devuelve una Promises con lo cual podemos manejar de 
     //ser necesario peticiones asincronas   
     getItems(): Promise<Product[]> {
+        //Obtenemos todos los items de la base de datos
+        this.db.getAll('items').then((items) => {
+            console.log(items);
+            this.items = items;
+        }, (error) => {
+            console.log(error);
+        });
       return Promise.resolve(this.items);
     }
 
@@ -56,8 +86,14 @@ export class ListService {
         //Agregamos al nuevo producto, el nombre y la cantidad informada por el usuario
         newProd.productName = newProduct.productName;
         newProd.cantidad = newProduct.cantidad;
-        //Agregamos en nuevo producto a la lista
-        this.items.push(newProd);
+
+        //Agregamos el nuevo item (producto, cantidad) a la IndexedDB 
+        this.db.add('items', newProd, null).then(() => {
+            //Agregamos en nuevo producto a la lista
+            this.items.push(newProd);
+        }, (error) => {
+            console.log(error);
+        });
         //resolvemos la Promise retornando los items activo incluyendo el item agreagdo
         return Promise.resolve(this.items);
     }
